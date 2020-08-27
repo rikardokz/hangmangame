8ff4c5e3b07917090fd18a21e2291b71084185a7
Ricardo Cardoso <rikardo_kz@live.com.pt>
2020-08-27 18:28:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
 const wordEl = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
-const popup = document.getElementById("popup-container");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.querySelector(".popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
 
-const figureParts = document.querySelectorAll("figure-part");
+const figureParts = document.querySelectorAll(".figure-part");
 
-const words = [
-  "Ativista",
-  "Professor",
-  "Minuto",
-  "Luta",
-  "Guidão",
-  "Lapso",
-  "Cadeia",
-  "Caro",
-  "Tela",
-  "Cuba",
-  "Embaixador",
-  "Jardim",
-  "Ir",
-  "Outono",
-  "Promotor",
-  "Greenpeace",
-  "Alma",
-  "Ainda",
-  "Consumir",
-  "Ofensor",
-  "Atmosfera",
-  "Gancho",
-  "Puro",
-  "Desviar",
-  "Frágil",
-  "Estréia",
-  "Refúgio",
-  "Entorpecido",
-  "Onde",
-  "Superar",
-];
+const words = ["ativista", "professor", "minuto", "luta", "superar"];
 
-let selectedWord = words[Math.floor(Math.random() * words.length)];
+let selectedWord = words[
+  Math.floor(Math.random() * words.length)
+].toLowerCase();
 
-const correctLetters = ["i"];
+const correctLetters = [];
 const wrongLetters = [];
 
 // mostra a palavra escondida
@@ -59,11 +30,76 @@ function displayWord() {
       .join("")}
         `;
 
-  const innerWord = wordEl.innerText.replace(/\n/g, " ");
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
 
-  if (innerWord === selectedWord) {
+  if (innerWord === selectedWord.toLowerCase()) {
     finalMessage.innerText = "Parabéns! Ganhaste!";
+    popup.style.display = "flex";
   }
 }
+
+// update the wrong letters
+function updateWrongLettersEl() {
+  // update where it says wrong
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+  // add figure
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+  // se if figure is done and show pop up
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Infelizmente Perdeste!";
+    popup.style.display = "flex";
+  }
+}
+
+// show notification that the letter was already used
+function showNotification() {
+  notification.classList.add("show"),
+    setTimeout(() => notification.classList.remove("show"), 3000);
+}
+
+// keydown letter press
+window.addEventListener("keydown", (e) => {
+  if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode === 192) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+playAgainBtn.addEventListener("click", () => {
+  wrongLetters.length = 0;
+  correctLetters.length = 0;
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+  popup.style.display = "none";
+  // figureParts.style.display = "none";
+});
 
 displayWord();
